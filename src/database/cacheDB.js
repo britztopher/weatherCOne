ec7d42a659a5e6bec3bdfeb3d671683e78d7cdf8
Chester Copperpot <britztopher@gmail.com>
2019-09-26 21:19:53 -0400

    wiping db clean of test data

--- src/database/cacheDB.js
+++ src/database/cacheDB.js
@@ -1,24 +1,3 @@
-let db = [[{
-  timestamp: '2015-09-01T16:00:00.000Z',
-  temperature: 27.1,
-  dewPoint: 16.9
-},
-{ timestamp: '2015-09-01T16:10:00.000Z', temperature: 27.3 },
-{
-  timestamp: '2015-09-01T16:20:00.000Z',
-  temperature: 27.5,
-  dewPoint: 17.1
-},
-{
-  timestamp: '2015-09-01T16:30:00.000Z',
-  temperature: 27.4,
-  dewPoint: 17.3
-},
-{ timestamp: '2015-09-01T16:40:00.000Z', temperature: 27.2 },
-{
-  timestamp: '2015-09-01T17:00:00.000Z',
-  temperature: 28.1,
-  dewPoint: 18.3
-}]]
+let db = []
 
 export default db;
